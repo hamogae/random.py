c21d47ccfe1094a3182f02555cbaeb121a7e12b4
hayun <102849597+hmogae@users.noreply.github.com>
2022-04-24 14:25:27 +0900

    Create r3.py

---
+++ randomtype/r3.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+LENGTH = 15
+string_pool = string.ascii_lowercase
+result = ""
+for i in range(LENGTH) :
+    result += random.choice(string_pool)
+print(result)
+
+# string.digits (0123456789)
+# string.ascii_lowercase (영어, 소문자)
+# string.ascii_uppercase (영어, 대문자)
+# string.ascii_letters (영어 대소문자)
+# string.punctuation (기호)
