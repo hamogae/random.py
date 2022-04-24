35e660ff26e2978ab56a0dbbc13846e044706543
hayun <102849597+hmogae@users.noreply.github.com>
2022-04-24 14:22:24 +0900

    random.cmd (실행파일)

---
+++ random.cmd
@@ -0,0 +1,136 @@
+@echo off
+cls
+color b
+echo.
+echo ======================== (10개)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x2)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x3)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x4)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x5)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x6)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x7)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x8)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x9)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo ======================== (10개 x10)
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+py wifi1.py
+echo ========================
+echo.
+echo 출력이 완료되었어요!
+pause
